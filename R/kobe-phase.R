f5976d9eeed21435b34f80a7b94fd7bd10315495
lauriekell <laurie@kell.es>
2019-01-02 23:51:16 +0000

    tidy

--- R/kobe-phase.R
+++ R/kobe-phase.R
@@ -3,10 +3,11 @@
 utils::globalVariables(c("x","y","fill"))
 
 ### provide a back drop on which to overlay data
-kobePhaseFn=function(object,xlim,ylim,quadcol=c("yellow","green","red")){    
-  quads<- rbind(data.frame(x=c(-Inf,-Inf,Inf,Inf), y=c(-Inf,Inf,Inf,-Inf), fill=as.factor(quadcol[1])),
-                data.frame(x=c(   1,   1,Inf,Inf), y=c(-Inf,  1,  1,-Inf), fill=as.factor(quadcol[2])),
-                data.frame(x=c(-Inf,-Inf,  1,  1), y=c(   1,Inf,Inf,   1), fill=as.factor(quadcol[3])))
+kobePhaseFn=function(object,xlim,ylim,quadcol=c("yellow","green","red","yellow")){    
+  quads<- rbind(data.frame(x=c(-Inf,-Inf, Inf,Inf), y=c(-Inf,Inf,Inf,-Inf), fill=as.factor(quadcol[1])),
+                data.frame(x=c(   1,   1,Inf,Inf), y=c(-Inf,  1,  1, -Inf), fill=as.factor(quadcol[2])),
+                data.frame(x=c(-Inf,-Inf,  1,  1), y=c(   1,Inf,Inf,    1), fill=as.factor(quadcol[3])),
+                data.frame(x=c(   1,   1,Inf,Inf), y=c(   1,Inf,Inf,    1), fill=as.factor(quadcol[4])))
   
   p=ggplot(object)+geom_polygon(data=quads,aes(x,y,fill=fill)) +
     scale_fill_manual(values = quadcol, guide="none") +
@@ -36,14 +37,14 @@ kobePhaseFn=function(object,xlim,ylim,quadcol=c("yellow","green","red")){
 #' @examples
 #' \dontrun{kobePhase()}
 setMethod('kobePhase', signature(object='missing'),
-  function(object,xlim=c(0,2),ylim=xlim,quadcol=c("yellow","green","red")){
+  function(object,xlim=c(0,2),ylim=xlim,quadcol=c("yellow","green","red","yellow")){
    
        invisible(kobePhaseFn(NULL,xlim,ylim,quadcol=quadcol))})
 
 setMethod('kobePhase', signature(object='data.frame'),
   function(object,xlim=c(0,ceiling(2*max(object$stock,  na.rm=TRUE))/2),
                   ylim=c(0,ceiling(2*max(object$harvest,na.rm=T))/2),
-                  quadcol=c("yellow","green","red")){
+                  quadcol=c("yellow","green","red","yellow")){
     
        invisible(kobePhaseFn(object,xlim,ylim,quadcol=quadcol))})
 
